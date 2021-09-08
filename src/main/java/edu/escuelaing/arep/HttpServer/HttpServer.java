337b8b9ec7bb2fd8d95cf497b7dd34d6d3319536
Sebastian Caceres <sebastiancaceres98@hotmail.com>
2021-09-08 16:17:37 -0500

    finaltest

--- src/main/java/edu/escuelaing/arep/HttpServer/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer/HttpServer.java
@@ -57,7 +57,7 @@ public class HttpServer {
             UrlStr = request.get(0).split(" ")[1];
         }
         if (UrlStr.equals("/clima")){
-            outputLine="";
+            outputLine=RespuestaClima();
             printWriter.println(outputLine);
         }
         else if(UrlStr.contains("/consulta?lugar=")){
@@ -68,6 +68,39 @@ public class HttpServer {
 
         }
         printWriter.close();
+    }
+
+    public string RespuestaClima(){
+        String outputLine +=   "<!DOCTYPE html>\n
+                <html>\n
+                       <head>\n
+                           <title>Weather Consult</title>\n
+                           <meta charset=\"UTF-8\">\n
+                           <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.8\">
+                       </head>\n
+                       <body>\n
+                       <div class='container center'>\n
+                           <div class='card'>\n
+                           <h1> <span class='dot'></span> CONSULT THE WHEATER API HERE <span class='dot'></span></h1>\n
+                           <p>Type here the city you want to consult</p>\n
+                           <div class='info-wrapper'>\n
+                           <input id='inputField'>\n
+                           <button id='submit'>Sent it</button>\n
+                       </div>\n
+                       <p><strong>Side Note: </strong> To go back from the resource just erase the last part of the URL until the last / inclusive</p>\n
+                      </div>\n"
+                      </body>\n"
+                       <script>\n"
+                           document.addEventListener('DOMContentLoaded', () => {\n
+                            document.getElementById('submit').addEventListener('click', () => {\n
+                        var request = 'consulta?lugar=' + document.getElementById('inputField').value\n
+                        window.location.replace(request);\n
+                    "})\n
+                "});\n
+                       </script>
+                </html>;
+
+        return outputLine;
     }
 
 
